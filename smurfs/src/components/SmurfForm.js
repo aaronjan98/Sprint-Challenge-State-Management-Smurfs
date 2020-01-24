0328317a4974199b42706a436500db492d1da1d6
aaronjan <aaronjan98@gmail.com>
2020-01-24 12:05:27 -0800

    converted SmurfForm into class based component

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,68 +1,107 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
-const SmurfForm = () => {
+class SmurfForm extends Component {
 
-    const [smurf ,setSmurf] = useState({
-        name: '',
-        age: '',
-        height: ''
-    });
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+            age: '',
+            height: ''
+        }
+    }
 
-    const handleChanges = e => {
-        setSmurf( 
-            {...smurf, [e.target.name]: e.target.value} 
-            );
-            console.log('smurf', smurf);
-    };
-    
-    const handleSubmit = e => {
-        e.preventDefault();
-    };
+    changeHandler = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
 
-    const addItem = (e, smurf) => {
-        e.preventDefault();
+    submitHandler = e => {
+        e.preventDefault()
+        console.log('state object:', this.state)
         axios
-          .post('http://localhost:3333/smurfs', smurf)
+          .post('http://localhost:3333/smurfs', this.state)
           .then(res => {
-            this.setSmurf({
-              items: res.data
-            });
-            // HTTP STEP V - Clear data form in ItemForm and route to /item-list
-            this.props.history.push('/item-list');
+            console.log(res);
           })
           .catch(err => {
             console.log(err);
           });
-      };
+    }
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <input 
-                type="text"
-                name="name"
-                placeholder="name of smurf"
-                value={smurf.name}
-                onChange={handleChanges}
-            />
-            <input 
-                type="text"
-                name="age"
-                placeholder="age"
-                value={smurf.age}
-                onChange={handleChanges}
-            />
-            <input 
-                type="text"
-                name="height"
-                placeholder="height"
-                value={smurf.height}
-                onChange={handleChanges}
-            />
-            <button className='add-btn' onClick={addItem}>Add</button>
-
-        </form>
-    );
+    render() {
+        const { name, age, height } = this.state;
+        return (
+            <form onSubmit={this.submitHandler}>
+                <input 
+                    type="text"
+                    name="name"
+                    placeholder="name of smurf"
+                    value={name}
+                    onChange={this.changeHandler}
+                />
+                <input 
+                    type="text"
+                    name="age"
+                    placeholder="age"
+                    value={age}
+                    onChange={this.changeHandler}
+                />
+                <input 
+                    type="text"
+                    name="height"
+                    placeholder="height"
+                    value={height}
+                    onChange={this.changeHandler}
+                />
+                <button type="submit" onClick={this.addItem}>Submit</button>
+            </form>
+        )
+    }
 }
-
 export default SmurfForm;
+
+
+
+//     const [smurf ,setSmurf] = useState({
+//         name: '',
+//         age: '',
+//         height: ''
+//     });
+
+//     const handleChanges = e => {
+//         setSmurf( 
+//             {...smurf, [e.target.name]: e.target.value} 
+//             );
+//             console.log('smurf', smurf);
+//     };
+    
+//     const handleSubmit = (e, smurf) => {
+//         console.log('handleSubmit:', smurf);
+//         e.preventDefault();
+//     };
+
+//     const addItem = (e, smurf) => {
+//         console.log('SMURF', smurf);
+//         e.preventDefault();
+        // axios
+        //   .post('http://localhost:3333/smurfs', smurf)
+        //   .then(res => {
+        //       console.log('RES', res.data);
+        //     setSmurf({
+        //       name: res.data.name,
+        //       age: res.data.age,
+        //       height: res.data.height
+        //     });
+        //   })
+        //   .catch(err => {
+        //     console.log(err);
+        //   });
+//       };
+
+
+//     );
+// }
+
