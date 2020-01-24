56cf85a4558c20597b4db47c6ff7127dd02f9aae
aaronjan <aaronjan98@gmail.com>
2020-01-24 08:43:43 -0800

    Wrapped my smurf component w/ Provider

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,17 +1,22 @@
 import React, { useState, createContext } from "react";
 import "./App.css";
 import SmurfCard from "./SmurfCard";
+import { SmurfContext } from "../contexts/SmurfContext";
 
 const App = () => {
   const [name, setName] = useState();
+  const { Provider } = SmurfContext;
+  console.log('Smurf Context:', SmurfContext);
 
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-        <SmurfCard  name={name} />
+        <Provider value={name}>
+          <h1>SMURFS! 2.0 W/ Redux</h1>
+          <div>Welcome to your state management version of Smurfs!</div>
+          <div>Start inside of your `src/index.js` file!</div>
+          <div>Have fun!</div>
+          <SmurfCard/>
+        </Provider>
       </div>
     );
 }
