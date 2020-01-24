44348198d1f179b84cd4d427858437d80252064e
aaronjan <aaronjan98@gmail.com>
2020-01-24 08:30:55 -0800

    created Context and SmurfCard.js

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,19 @@
-import React, { Component } from "react";
+import React, { useState, createContext } from "react";
 import "./App.css";
-class App extends Component {
-  render() {
+import SmurfCard from "./SmurfCard";
+
+const App = () => {
+  const [name, setName] = useState();
+
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        <SmurfCard  name={name} />
       </div>
     );
-  }
 }
 
 export default App;
---
+++ smurfs/src/components/SmurfCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SmurfCard = () => {
+    return(
+        <h6>I'm the SmurfCard!</h6>
+    );
+}
+
+export default SmurfCard;
